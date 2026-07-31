e479bad82deae777fec60f51e1602251b1298f7f
connect-avijitpaul <connect.avijitpaul@gmail.com>
2026-07-31 16:21:04 +0600

    lenght-add

---
+++ lenght.js
@@ -0,0 +1,7 @@
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const number2 = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+// const allNumber = number.concat(number2);
+// console.log(allNumber);
+for (numbers of number) {
+  console.log(numbers);
+}
