34fc1c80d4ba9efbd8adc9c10051837695cc80c8
connect-avijitpaul <connect.avijitpaul@gmail.com>
2026-07-31 16:57:17 +0600

    math& loop

---
+++ loop.js
@@ -0,0 +1,7 @@
+// for (let i = 0; i <= 20; i++) {
+//   console.log('value of i', i);
+// }
+let number = 5;
+for (let i = 1; i <= 10; i = i + 2) {
+  console.log(number, 'X', i, '=', i * number);
+}
---
+++ point1.js
@@ -0,0 +1,50 @@
+const price1 = 500;
+const price2 = 600;
+
+if (price1 == price2) {
+  console.log('point1:' + 'condition == is true');
+} else {
+  console.log('point1:' + 'condition == is worng');
+}
+
+if (price1 === price2) {
+  console.log('point2:' + 'condition === is true');
+} else {
+  console.log('point2:' + 'condition === is worng');
+}
+
+if (price1 != price2) {
+  console.log('point3:' + 'condition != true');
+} else {
+  console.log('point3:' + 'condition != worng');
+}
+
+if (price1 !== price2) {
+  console.log('point4:' + 'console !== is true');
+} else {
+  console.log('point4:' + 'condition !=== is worng');
+}
+
+if (price1 > price2) {
+  console.log('point5:' + 'condition > is true');
+} else {
+  console.log('point5:' + 'condition > is worng');
+}
+
+if (price1 < price2) {
+  console.log('point6:' + 'condition < is true');
+} else {
+  console.log('point6:' + 'condition < is worng');
+}
+
+if (price1 <= price2) {
+  console.log('point6:' + 'condition < is true');
+} else {
+  console.log('point6:' + 'condition < is worng');
+}
+
+if (price1 >= price2) {
+  console.log('point5:' + 'condition > is true');
+} else {
+  console.log('point5:' + 'condition > is worng');
+}
